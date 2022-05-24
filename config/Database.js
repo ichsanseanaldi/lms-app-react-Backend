d938f15c9b70bd1a64cb82b12867188858ea6474
Ichsan Seanaldi <ichsanseanaldi10@gmail.com>
2022-05-24 22:06:54 +0700

    db config

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('lms_db', 'postgres', 'update123', {
-    host: 'localhost',
+const db = new Sequelize('d62be2516j9ffi', 'zdkduqsvjvustq', '022b4240a77c98cd366757f70f628718df7d61b9575e730a0ae9c4ef644044a5', {
+    host: 'ec2-52-54-212-232.compute-1.amazonaws.com',
     dialect: 'postgres',
     define: {
         timestamps: false
